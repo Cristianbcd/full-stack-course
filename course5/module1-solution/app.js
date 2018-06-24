43d237a59e2d2db31759436139e447ba349ef5b2
Cristian Rodriguez <13244760+Cristianbcd@users.noreply.github.com>
2018-06-24 00:02:40 -0500

    Solved

--- course5/module1-solution/app.js
+++ course5/module1-solution/app.js
@@ -11,8 +11,6 @@
 		$scope.checkIfTooMuch = function () {
 
 			var arrayListOfFood = $scope.listOfFood.split(',');
-			console.log(arrayListOfFood.length);
-			console.log(arrayListOfFood.length);
 			if(arrayListOfFood.length == 1 && arrayListOfFood[0] == ""){
 				$scope.message = "Please enter data first";
 			} else {
