420d3ba84c18bb8f67f113144326c372a3bf864b
Cristian Rodriguez <cristian.rodriguez@bayteq.com>
2018-07-02 12:57:30 -0500

    Added AngularJS Framework

---
+++ course5/module2-solution/app.js
@@ -0,0 +1,25 @@
+(function () {
+	'use strict';
+	angular.module('ShoppingListCheckOff', []).controller('LunchCheckController', LunchCheckController);
+
+	LunchCheckController.$inject = ['$scope', '$filter'];
+
+	function LunchCheckController($scope, $filter){
+
+		$scope.listOfFood = "";
+
+		$scope.checkIfTooMuch = function () {
+
+			var arrayListOfFood = $scope.listOfFood.split(',');
+			if(arrayListOfFood.length == 1 && arrayListOfFood[0] == ""){
+				$scope.message = "Please enter data first";
+			} else {
+				if(arrayListOfFood.length <= 3){
+					$scope.message = "Enjoy!";
+				} else {
+					$scope.message = "Too much!";
+				}
+			}
+		}
+	}
+})();
